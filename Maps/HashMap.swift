2e875b85ef422ffe2397f0ee2f3fcca6446b104b
David Gerard <David@davids-macbook-air-4.local>
2018-10-30 14:23:10 -0400

    make count in hash map more efficient

--- Maps/HashMap.swift
+++ Maps/HashMap.swift
@@ -13,6 +13,7 @@ struct HashMap<K: Hashable, V>: CustomStringConvertible {
     var values: [V?]
     var linearMap = LinearMap<K, V>()
     let initialArraySize: Int
+    var count = 0
     var numberCollisions = 0
     
     init(initialArraySize: Int = 100) {
@@ -34,9 +35,11 @@ struct HashMap<K: Hashable, V>: CustomStringConvertible {
         } else if keys[index] == nil { //if not present + space empty, add entry
             keys[index] = k
             values[index] = v
+            count += 1
         } else { //if space occupied by other, place new entry in linear map
             numberCollisions += 1
             linearMap[k] = v
+            count += 1
         }
     }
     
@@ -50,10 +53,6 @@ struct HashMap<K: Hashable, V>: CustomStringConvertible {
             return linearMap[k]
         }
     }
-    
-    var count: Int {    //include hash and linear maps
-        return getNumberCollisions() + linearMap.count
-    }//return keys.reduce(0, {$0 + ($1 != nil ? 1 : 0)}) + linearMap.count
     
     var description: String {
         var desc = "[\n"
